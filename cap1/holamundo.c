bfeb6e48aa0006755905217d1ac4f08e5c791dee
kjiron <jiron.kevin.a@gmail.com>
2020-08-21 18:26:23 -0600

    Mi primer commit para el cap1

---
+++ cap1/holamundo.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //libreria que me permite utilizar la funcion puts
+
+int main(void) // funcion que es entera y necesita devolver un entero, es el main
+{
+	puts("Hola mundo"); // dentro de puts va el string que quiero ver en pantalla
+	return 0; // entero que devuelvo
+}
